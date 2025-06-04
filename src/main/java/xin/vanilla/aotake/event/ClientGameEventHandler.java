cadd219e8c4aa377a1c053a5141b1f1be1cc8c52
Tsuki <TsukiMaaii@gmail.com>
2025-06-04 18:00:23 +0800

    chore 1.添加临时logo

--- src/main/java/xin/vanilla/aotake/event/ClientGameEventHandler.java
+++ src/main/java/xin/vanilla/aotake/event/ClientGameEventHandler.java
@@ -3,6 +3,7 @@ package xin.vanilla.aotake.event;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.Tooltip;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.ContainerScreen;
 import net.minecraft.world.entity.Entity;
 import net.minecraftforge.api.distmarker.Dist;
@@ -129,9 +130,10 @@ public class ClientGameEventHandler {
 
     @SubscribeEvent
     public static void onRenderScreen(ScreenEvent event) {
-        if (event.getScreen() instanceof ContainerScreen
+        Screen screen = event.getScreen();
+        if (screen instanceof ContainerScreen
                 && Minecraft.getInstance().player != null
-                && event.getScreen().getTitle().getString()
+                && screen.getTitle().getString()
                 .startsWith(MOD_NAME.toTextComponent(AotakeUtils.getPlayerLanguage(Minecraft.getInstance().player)).getString())
         ) {
             if (event instanceof ScreenEvent.Init.Post eve) {
@@ -140,7 +142,7 @@ public class ClientGameEventHandler {
                         new Button.Builder(Component.literal("✕").setColor(EnumMCColor.RED.getColor()).toTextComponent()
                                 , button -> AotakeUtils.sendPacketToServer(new ClearDustbinNotice(true, true)))
                                 .size(20, 20)
-                                .pos(event.getScreen().width / 2 - 88 - 21, event.getScreen().height / 2 - 111)
+                                .pos(screen.width / 2 - 88 - 21, screen.height / 2 - 111)
                                 .tooltip(Tooltip.create(Component.translatable(EnumI18nType.MESSAGE, "clear_cache").toTextComponent(AotakeUtils.getClientLanguage())))
                                 .build()
                 );
@@ -149,7 +151,7 @@ public class ClientGameEventHandler {
                         new Button.Builder(Component.literal("✕").setColor(EnumMCColor.RED.getColor()).toTextComponent()
                                 , button -> AotakeUtils.sendPacketToServer(new ClearDustbinNotice(true, false)))
                                 .size(20, 20)
-                                .pos(event.getScreen().width / 2 - 88 - 21, event.getScreen().height / 2 - 90)
+                                .pos(screen.width / 2 - 88 - 21, screen.height / 2 - 90)
                                 .tooltip(Tooltip.create(Component.translatable(EnumI18nType.MESSAGE, "clear_all_dustbin").toTextComponent(AotakeUtils.getClientLanguage())))
                                 .build()
                 );
@@ -158,7 +160,7 @@ public class ClientGameEventHandler {
                         new Button.Builder(Component.literal("✕").setColor(EnumMCColor.YELLOW.getColor()).toTextComponent()
                                 , button -> AotakeUtils.sendPacketToServer(new ClearDustbinNotice(false, false)))
                                 .size(20, 20)
-                                .pos(event.getScreen().width / 2 - 88 - 21, event.getScreen().height / 2 - 69)
+                                .pos(screen.width / 2 - 88 - 21, screen.height / 2 - 69)
                                 .tooltip(Tooltip.create(Component.translatable(EnumI18nType.MESSAGE, "clear_cur_dustbin").toTextComponent(AotakeUtils.getClientLanguage())))
                                 .build()
                 );
@@ -167,7 +169,7 @@ public class ClientGameEventHandler {
                         new Button.Builder(Component.literal("↻").toTextComponent()
                                 , button -> AotakeUtils.sendPacketToServer(new OpenDustbinNotice(0)))
                                 .size(20, 20)
-                                .pos(event.getScreen().width / 2 - 88 - 21, event.getScreen().height / 2 - 48)
+                                .pos(screen.width / 2 - 88 - 21, screen.height / 2 - 48)
                                 .tooltip(Tooltip.create(Component.translatable(EnumI18nType.MESSAGE, "refresh_page").toTextComponent(AotakeUtils.getClientLanguage())))
                                 .build()
                 );
@@ -176,7 +178,7 @@ public class ClientGameEventHandler {
                         new Button.Builder(Component.literal("▲").toTextComponent()
                                 , button -> AotakeUtils.sendPacketToServer(new OpenDustbinNotice(-1)))
                                 .size(20, 20)
-                                .pos(event.getScreen().width / 2 - 88 - 21, event.getScreen().height / 2 - 27)
+                                .pos(screen.width / 2 - 88 - 21, screen.height / 2 - 27)
                                 .tooltip(Tooltip.create(Component.translatable(EnumI18nType.MESSAGE, "previous_page").toTextComponent(AotakeUtils.getClientLanguage())))
                                 .build()
                 );
@@ -185,7 +187,7 @@ public class ClientGameEventHandler {
                         new Button.Builder(Component.literal("▼").toTextComponent()
                                 , button -> AotakeUtils.sendPacketToServer(new OpenDustbinNotice(1)))
                                 .size(20, 20)
-                                .pos(event.getScreen().width / 2 - 88 - 21, event.getScreen().height / 2 - 6)
+                                .pos(screen.width / 2 - 88 - 21, screen.height / 2 - 6)
                                 .tooltip(Tooltip.create(Component.translatable(EnumI18nType.MESSAGE, "next_page").toTextComponent(AotakeUtils.getClientLanguage())))
                                 .build()
                 );
--- src/main/java/xin/vanilla/aotake/network/packet/OpenDustbinNotice.java
+++ src/main/java/xin/vanilla/aotake/network/packet/OpenDustbinNotice.java
@@ -4,6 +4,7 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.event.network.CustomPayloadEvent;
 import xin.vanilla.aotake.AotakeSweep;
+import xin.vanilla.aotake.config.CommonConfig;
 import xin.vanilla.aotake.data.world.WorldTrashData;
 import xin.vanilla.aotake.util.AotakeUtils;
 
@@ -28,9 +29,12 @@ public class OpenDustbinNotice {
             if (player != null) {
                 String playerUUID = AotakeUtils.getPlayerUUIDString(player);
                 Integer page = AotakeSweep.getPlayerDustbinPage().getOrDefault(playerUUID, 1);
-                player.closeContainer();
-                int result = player.openMenu(WorldTrashData.getTrashContainer(player, page + packet.offset)).orElse(0);
-                if (result > 0) AotakeSweep.getPlayerDustbinPage().put(playerUUID, page + packet.offset);
+                int i = page + packet.offset;
+                if (i > 0 && i <= CommonConfig.DUSTBIN_PAGE_LIMIT.get()) {
+                    player.closeContainer();
+                }
+                int result = player.openMenu(WorldTrashData.getTrashContainer(player, i)).orElse(0);
+                if (result > 0) AotakeSweep.getPlayerDustbinPage().put(playerUUID, i);
             }
         });
         ctx.setPacketHandled(true);
