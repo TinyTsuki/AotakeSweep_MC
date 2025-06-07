3e3cddfb11a57769a97aa43631d017cff239a150
Tsuki <TsukiMaaii@gmail.com>
2025-06-07 12:27:53 +0800

    chore 1.修改玩家语言与虚拟权限信息配置存储方式

--- src/main/java/xin/vanilla/aotake/data/player/IPlayerSweepData.java
+++ src/main/java/xin/vanilla/aotake/data/player/IPlayerSweepData.java
@@ -1,34 +1,14 @@
 package xin.vanilla.aotake.data.player;
 
-import lombok.NonNull;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.util.INBTSerializable;
 
-import javax.annotation.Nullable;
-
 /**
- * 玩家传送数据
+ * 玩家数据
  */
 public interface IPlayerSweepData extends INBTSerializable<CompoundTag> {
-
-    /**
-     * 获取语言
-     */
-    String getLanguage();
-
-    /**
-     * 设置语言
-     */
-    void setLanguage(String language);
-
-    /**
-     * 获取有效的语言
-     */
-    @NonNull
-    String getValidLanguage(@Nullable Player player);
 
     boolean isNotified();
 
