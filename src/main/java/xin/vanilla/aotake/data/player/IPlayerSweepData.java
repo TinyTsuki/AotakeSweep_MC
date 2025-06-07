b3040fc6de5cb3fc394b2f7de5c4e26acdc55edd
Tsuki <TsukiMaaii@gmail.com>
2025-06-07 12:27:53 +0800

    chore 1.修改玩家语言与虚拟权限信息配置存储方式

--- src/main/java/xin/vanilla/aotake/data/player/IPlayerSweepData.java
+++ src/main/java/xin/vanilla/aotake/data/player/IPlayerSweepData.java
@@ -1,36 +1,16 @@
 package xin.vanilla.aotake.data.player;
 
-import lombok.NonNull;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.capabilities.AutoRegisterCapability;
 import net.minecraftforge.common.util.INBTSerializable;
 
-import javax.annotation.Nullable;
-
 /**
- * 玩家传送数据
+ * 玩家数据
  */
 @AutoRegisterCapability
 public interface IPlayerSweepData extends INBTSerializable<CompoundTag> {
-
-    /**
-     * 获取语言
-     */
-    String getLanguage();
-
-    /**
-     * 设置语言
-     */
-    void setLanguage(String language);
-
-    /**
-     * 获取有效的语言
-     */
-    @NonNull
-    String getValidLanguage(@Nullable Player player);
 
     boolean isNotified();
 
--- src/main/java/xin/vanilla/aotake/data/player/PlayerSweepData.java
+++ src/main/java/xin/vanilla/aotake/data/player/PlayerSweepData.java
@@ -1,20 +1,14 @@
 package xin.vanilla.aotake.data.player;
 
-import lombok.NonNull;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
-import xin.vanilla.aotake.util.AotakeUtils;
-
-import javax.annotation.Nullable;
 
 /**
- * 玩家传送数据
+ * 玩家数据
  */
 public class PlayerSweepData implements IPlayerSweepData {
-    private String language = "client";
     /**
      * 是否已发送使用说明
      */
@@ -23,22 +17,6 @@ public class PlayerSweepData implements IPlayerSweepData {
      * 是否显示清理结果
      */
     private boolean showSweepResult = true;
-
-    @Override
-    public String getLanguage() {
-        return this.language;
-    }
-
-    @Override
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
-    @NonNull
-    @Override
-    public String getValidLanguage(@Nullable Player player) {
-        return AotakeUtils.getValidLanguage(player, this.getLanguage());
-    }
 
     @Override
     public boolean isNotified() {
@@ -61,19 +39,16 @@ public class PlayerSweepData implements IPlayerSweepData {
     }
 
     public void writeToBuffer(FriendlyByteBuf buffer) {
-        buffer.writeUtf(this.getLanguage());
         buffer.writeBoolean(this.notified);
         buffer.writeBoolean(this.showSweepResult);
     }
 
     public void readFromBuffer(FriendlyByteBuf buffer) {
-        this.language = buffer.readUtf();
         this.notified = buffer.readBoolean();
         this.showSweepResult = buffer.readBoolean();
     }
 
     public void copyFrom(IPlayerSweepData capability) {
-        this.language = capability.getLanguage();
         this.notified = capability.isNotified();
         this.showSweepResult = capability.isShowSweepResult();
     }
@@ -81,7 +56,6 @@ public class PlayerSweepData implements IPlayerSweepData {
     @Override
     public CompoundTag serializeNBT(HolderLookup.Provider registryAccess) {
         CompoundTag tag = new CompoundTag();
-        tag.putString("language", this.getLanguage());
         tag.putBoolean("notified", this.notified);
         tag.putBoolean("showSweepResult", this.showSweepResult);
         return tag;
@@ -89,7 +63,6 @@ public class PlayerSweepData implements IPlayerSweepData {
 
     @Override
     public void deserializeNBT(HolderLookup.Provider registryAccess, CompoundTag nbt) {
-        this.setLanguage(nbt.getString("language"));
         this.notified = nbt.getBoolean("notified");
         this.showSweepResult = nbt.getBoolean("showSweepResult");
     }
