90629449e8d99593c83bc85daa546aa9aca846f3
Tsuki <TsukiMaaii@gmail.com>
2025-09-15 12:45:41 +0800

    feat 1.区块实体过多的提示添加点击补全传送指令

---
+++ src/main/java/xin/vanilla/aotake/util/CompatNarcissus.java
@@ -0,0 +1,8 @@
+package xin.vanilla.aotake.util;
+
+public class CompatNarcissus {
+    public static String getTpCommand() {
+        return xin.vanilla.narcissus.util.NarcissusUtils
+                .getCommand(xin.vanilla.narcissus.enums.EnumCommandType.TP_COORDINATE);
+    }
+}
