2dbb5f5644a37c69ae99d728712d2b5ce4d451a5
Tsuki <TsukiMaaii@gmail.com>
2025-09-27 00:09:52 +0800

    feat 1.添加配置项 实体清理模式 2.移除PlayerCapability并改为自定义PlayerDataStorage

--- src/main/java/xin/vanilla/aotake/enums/EnumChunkCheckMode.java
+++ src/main/java/xin/vanilla/aotake/enums/EnumChunkCheckMode.java
@@ -4,4 +4,26 @@ public enum EnumChunkCheckMode {
     DEFAULT,
     ADVANCED,
     ;
+
+    public static EnumChunkCheckMode valueOf(Object obj) {
+        if (obj instanceof EnumChunkCheckMode) return (EnumChunkCheckMode) obj;
+        if (obj instanceof String) {
+            for (EnumChunkCheckMode value : values()) {
+                if (value.name().equalsIgnoreCase((String) obj)) {
+                    return value;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static EnumChunkCheckMode valueOfOrDefault(Object obj) {
+        EnumChunkCheckMode value = valueOf(obj);
+        return value == null ? DEFAULT : value;
+    }
+
+    public static boolean isValid(Object obj) {
+        return valueOf(obj) != null;
+    }
+
 }
--- src/main/java/xin/vanilla/aotake/event/ClientModEventHandler.java
+++ src/main/java/xin/vanilla/aotake/event/ClientModEventHandler.java
@@ -3,8 +3,6 @@ package xin.vanilla.aotake.event;
 import net.minecraft.client.KeyMapping;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.ClientRegistry;
-import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -43,11 +41,6 @@ public class ClientModEventHandler {
         ClientRegistry.registerKeyBinding(DUSTBIN_KEY);
         ClientRegistry.registerKeyBinding(DUSTBIN_PRE_KEY);
         ClientRegistry.registerKeyBinding(DUSTBIN_NEXT_KEY);
-    }
-
-    @SubscribeEvent
-    public static void registerCaps(RegisterCapabilitiesEvent event) {
-        EventHandlerProxy.registerCaps(event);
     }
 
 }
