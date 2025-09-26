0ef128aa756aadbccc4b7bf3ca513dc78959560b
Tsuki <TsukiMaaii@gmail.com>
2025-09-27 00:09:52 +0800

    feat 1.添加配置项 实体清理模式 2.移除PlayerCapability并改为自定义PlayerDataStorage

--- src/main/java/xin/vanilla/aotake/event/ServerModEventHandler.java
+++ src/main/java/xin/vanilla/aotake/event/ServerModEventHandler.java
@@ -1,15 +1,12 @@
 package xin.vanilla.aotake.event;
 
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import xin.vanilla.aotake.AotakeSweep;
 
 /**
  * 服务端 Mod事件处理器
  */
-@Mod.EventBusSubscriber(modid = AotakeSweep.MODID, value = Dist.DEDICATED_SERVER, bus = Mod.EventBusSubscriber.Bus.MOD)
+// @Mod.EventBusSubscriber(modid = AotakeSweep.MODID, value = Dist.DEDICATED_SERVER, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ServerModEventHandler {
     private static final Logger LOGGER = LogManager.getLogger();
 
