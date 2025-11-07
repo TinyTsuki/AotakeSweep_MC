184a4070b95f173c6e4ee1a97dbf396a376ab50a
Tsuki <TsukiMaaii@gmail.com>
2025-11-08 03:44:51 +0800

    fix 1.首次打开垃圾桶时鼠标位置问题 2.垃圾箱应用模式使用混合模式时页数计算问题
    feat
    1.优化 配置相关指令
    2.移除 内置的语音资源包
    3.修改 chunkCheckRetain配置项为比例
    
    (cherry picked from commit 1acb86e9c926a5e54d5d2fdc6713a965d4f23840)

--- src/main/java/xin/vanilla/aotake/util/AotakeUtils.java
+++ src/main/java/xin/vanilla/aotake/util/AotakeUtils.java
@@ -821,7 +821,7 @@ public class AotakeUtils {
                                             .toTextComponent(language))
                                     )
                                     .setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND
-                                            , "/" + AotakeUtils.getCommandPrefix() + " config showSweepResult change")
+                                            , "/" + AotakeUtils.getCommandPrefix() + " config player showSweepResult change")
                                     )
                             )
                     );
