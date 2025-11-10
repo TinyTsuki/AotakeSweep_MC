c66395c3f0dfc3dd32de30469e930030482edb6e
Tsuki <TsukiMaaii@gmail.com>
2025-11-10 19:26:46 +0800

    feat 1.调整指令相关代码
    (cherry picked from commit 19c336bee994ea8457bef441381144c90197238a)

--- src/main/java/xin/vanilla/aotake/AotakeSweep.java
+++ src/main/java/xin/vanilla/aotake/AotakeSweep.java
@@ -31,6 +31,7 @@ import xin.vanilla.aotake.event.ClientModEventHandler;
 import xin.vanilla.aotake.network.ModNetworkHandler;
 import xin.vanilla.aotake.network.SplitPacket;
 import xin.vanilla.aotake.util.AotakeScheduler;
+import xin.vanilla.aotake.util.CommandUtils;
 import xin.vanilla.aotake.util.EntitySweeper;
 
 import java.util.*;
@@ -161,7 +162,7 @@ public class AotakeSweep {
         try {
             if (event.getConfig().getSpec() == ServerConfig.SERVER_CONFIG && serverInstance.val()) {
                 ServerConfig.bake();
-                AotakeCommand.configKeyMapCache.clear();
+                CommandUtils.configKeyMapCache.clear();
             }
         } catch (Exception ignored) {
         }
