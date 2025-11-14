ba772c4dfe76440742325a13de7f4117fef282c6
Tsuki <TsukiMaaii@gmail.com>
2025-11-14 15:56:47 +0800

    feat 1.添加 EntityData解析 2.恢复 被意外删掉的disable指令

--- src/main/java/xin/vanilla/aotake/util/AotakeUtils.java
+++ src/main/java/xin/vanilla/aotake/util/AotakeUtils.java
@@ -545,6 +545,7 @@ public class AotakeUtils {
     }
 
     public static List<Entity> getAllEntitiesByFilter(List<Entity> entities, boolean chuck) {
+        LOGGER.debug("Entity filter started at {}", System.currentTimeMillis());
         if (CollectionUtils.isNullOrEmpty(entities)) {
             entities = getAllEntities();
         }
@@ -554,6 +555,7 @@ public class AotakeUtils {
 
         List<Entity> filtered = entities.stream().filter(entity -> !(entity instanceof Player)).toList();
 
+        LOGGER.debug("Entity exceeded filter started at {}", System.currentTimeMillis());
         // 超限的非垃圾实体
         List<Entity> exceededEntityList = entities.stream()
                 // 非垃圾实体
@@ -564,6 +566,8 @@ public class AotakeUtils {
                 .filter(entry -> entry.getValue().size() > ServerConfig.ENTITY_LIST_LIMIT.get())
                 .flatMap(entry -> entry.getValue().stream())
                 .toList();
+
+        LOGGER.debug("Entity safe filter started at {}", System.currentTimeMillis());
 
         // 超限的安全实体
         List<Entity> exceededBlockList = filtered.stream()
@@ -581,6 +585,8 @@ public class AotakeUtils {
                 .flatMap(entry -> entry.getValue().stream())
                 .toList();
 
+        LOGGER.debug("Entity junk filter started at {}", System.currentTimeMillis());
+
         // 过滤
         Predicate<Entity> predicate = entity -> {
             boolean unsafe = !isSafeEntity(blockStateCache, entity);
@@ -593,7 +599,9 @@ public class AotakeUtils {
                     || exceededBlockList.contains(entity);
         };
 
-        return filtered.stream().filter(predicate).collect(Collectors.toList());
+        List<Entity> entityList = filtered.stream().filter(predicate).collect(Collectors.toList());
+        LOGGER.debug("Entity filter finished at {}", System.currentTimeMillis());
+        return entityList;
     }
 
     public static void sweep() {
