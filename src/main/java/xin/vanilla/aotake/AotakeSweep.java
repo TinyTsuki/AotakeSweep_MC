36e3bd13940d3237d63ad3cc754d7aecb176f048
Tsuki <TsukiMaaii@gmail.com>
2025-11-15 11:24:10 +0800

    fix 1.单机切换存档后垃圾无法进入垃圾箱

--- src/main/java/xin/vanilla/aotake/AotakeSweep.java
+++ src/main/java/xin/vanilla/aotake/AotakeSweep.java
@@ -141,6 +141,7 @@ public class AotakeSweep {
     }
 
     private void onServerStarting(FMLServerStartingEvent event) {
+        entitySweeper.clear();
         AotakeSweep.serverInstance.setKey(event.getServer()).setValue(true);
     }
 
