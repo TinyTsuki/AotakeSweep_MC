1b1e5e820fa2962d9bef5c047fc3cfd7cc2d5c48
Tsuki <TsukiMaaii@gmail.com>
2025-11-29 18:59:10 +0800

    feat 1.清扫间隔为0时不进行主动清扫

--- src/main/java/xin/vanilla/aotake/event/EventHandlerProxy.java
+++ src/main/java/xin/vanilla/aotake/event/EventHandlerProxy.java
@@ -66,6 +66,7 @@ public class EventHandlerProxy {
 
         long now = System.currentTimeMillis();
         long countdown = nextSweepTime - now;
+        long sweepInterval = ServerConfig.SWEEP_INTERVAL.get();
 
         // 扫地前提示
         String warnKey = String.valueOf(countdown / 1000);
@@ -100,8 +101,8 @@ public class EventHandlerProxy {
         }
 
         // 扫地
-        if (countdown <= 0) {
-            nextSweepTime = now + ServerConfig.SWEEP_INTERVAL.get();
+        if (countdown <= 0 && sweepInterval > 0) {
+            nextSweepTime = now + sweepInterval;
             AotakeScheduler.schedule(server, 1, AotakeUtils::sweep);
             // 给已安装mod玩家同步扫地倒计时
             for (String uuid : AotakeSweep.getCustomConfigStatus()) {
