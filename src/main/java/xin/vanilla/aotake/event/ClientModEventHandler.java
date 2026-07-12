e39b51eb704d7d4efdbbe5ed5ed895f86c914325
辉小月 <TsukiMaaii@gmail.com>
2026-07-12 18:20:24 +0800

    Use stable input registration API

--- src/main/java/xin/vanilla/aotake/event/ClientModEventHandler.java
+++ src/main/java/xin/vanilla/aotake/event/ClientModEventHandler.java
@@ -3,9 +3,9 @@ package xin.vanilla.aotake.event;
 import org.lwjgl.glfw.GLFW;
 import xin.vanilla.aotake.AotakeSweep;
 import xin.vanilla.aotake.notification.AotakeNotificationTypes;
+import xin.vanilla.banira.api.client.BaniraInput;
 import xin.vanilla.banira.api.client.BaniraKeyHandle;
 import xin.vanilla.banira.client.notification.NotificationTypeRegistry;
-import xin.vanilla.banira.client.util.BaniraKeyBindings;
 
 /**
  * 客户端：Banira 键位入队 + {@link xin.vanilla.banira.client.event.BaniraClientEventHub} 回调注册（不在此类上使用 Forge {@code @SubscribeEvent}）
@@ -15,20 +15,20 @@ public final class ClientModEventHandler {
     /**
      * 垃圾箱快捷键
      */
-    public static BaniraKeyHandle DUSTBIN_KEY = BaniraKeyBindings.register(AotakeSweep.MODID, "open_dustbin", GLFW.GLFW_KEY_UNKNOWN);
+    public static BaniraKeyHandle DUSTBIN_KEY = BaniraInput.registerKey(AotakeSweep.MODID, "open_dustbin", GLFW.GLFW_KEY_UNKNOWN);
     /**
      * 垃圾箱上页快捷键
      */
-    public static BaniraKeyHandle DUSTBIN_PRE_KEY = BaniraKeyBindings.register(AotakeSweep.MODID, "open_dustbin_pre", GLFW.GLFW_KEY_LEFT);
+    public static BaniraKeyHandle DUSTBIN_PRE_KEY = BaniraInput.registerKey(AotakeSweep.MODID, "open_dustbin_pre", GLFW.GLFW_KEY_LEFT);
     /**
      * 垃圾箱下页快捷键
      */
-    public static BaniraKeyHandle DUSTBIN_NEXT_KEY = BaniraKeyBindings.register(AotakeSweep.MODID, "open_dustbin_next", GLFW.GLFW_KEY_RIGHT);
+    public static BaniraKeyHandle DUSTBIN_NEXT_KEY = BaniraInput.registerKey(AotakeSweep.MODID, "open_dustbin_next", GLFW.GLFW_KEY_RIGHT);
 
     /**
      * 切换进度条显示按键
      */
-    public static BaniraKeyHandle PROGRESS_KEY = BaniraKeyBindings.register(AotakeSweep.MODID, "progress", GLFW.GLFW_KEY_TAB);
+    public static BaniraKeyHandle PROGRESS_KEY = BaniraInput.registerKey(AotakeSweep.MODID, "progress", GLFW.GLFW_KEY_TAB);
 
     private ClientModEventHandler() {
     }
