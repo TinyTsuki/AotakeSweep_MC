2a0bf7bfbad05a4d08b47a4530bac9b0cbc79581
辉小月 <TsukiMaaii@gmail.com>
2026-07-20 19:21:44 +0800

    Fix Fabric dustbin page boundaries

---
+++ src/test/java/xin/vanilla/aotake/util/DustbinPageNavigationTest.java
@@ -0,0 +1,24 @@
+package xin.vanilla.aotake.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class DustbinPageNavigationTest {
+    @Test
+    public void rejectsUnknownAndOutOfRangePages() {
+        assertFalse(DustbinPageNavigation.canNavigate(-1, -1, -1));
+        assertFalse(DustbinPageNavigation.canNavigate(1, 3, -1));
+        assertFalse(DustbinPageNavigation.canNavigate(3, 3, 1));
+        assertEquals(-1, DustbinPageNavigation.targetPage(1, 3, -1));
+    }
+
+    @Test
+    public void acceptsRefreshAndValidNavigation() {
+        assertEquals(1, DustbinPageNavigation.targetPage(1, 3, 0));
+        assertEquals(2, DustbinPageNavigation.targetPage(1, 3, 1));
+        assertTrue(DustbinPageNavigation.canNavigate(2, 3, -1));
+    }
+}
